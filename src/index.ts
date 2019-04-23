77a87ee3c81a69d695bb7070a772a6495624c3d4
Cédric Chantepie <chantepie@altern.org>
2019-04-23 17:07:35 +0200

    Fix handler when naming is ok on open

--- src/index.ts
+++ src/index.ts
@@ -57,7 +57,7 @@ export = (app: Application) => {
     const m = c.match(pr.title, config)
 
     if (m.isNone()) {
-      context.log(`Title of pull request #${pr.number} match configuration`)
+      return context.log(`Title of pull request #${pr.number} match configuration`)
     }
 
     const data: StatusData = m.fold(successData, msg => {
